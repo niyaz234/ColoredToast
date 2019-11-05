4818756a31682462f91e3aa0e39b88e74dd25d02
Niyaz <niyaz@divum.in>
2019-11-05 16:56:35 +0530

    first push

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text, TextInput, View, Animated, Platform, AsyncStorage} from "react-native";
+import {Root} from "native-base";
+
+export default class index extends React.Component{
+    render(){
+        <View style={{alignContent:"center",justifyContent:"center"}}>
+            <Text style={{color:"red", fontSize:50}}>NIYAZ</Text>
+        </View>
+    }
+}
